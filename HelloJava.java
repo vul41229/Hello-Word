340494a6324c447fa58bf1f9564191fd3a0e8166
vul41229 <111473707+vul41229@users.noreply.github.com>
2022-08-18 10:39:23 +0800

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+package day01;// 包名 叫day01 ---------樓和單元 ,作用:區分不同的房子
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.print(555);
+    }
+
+}
+
